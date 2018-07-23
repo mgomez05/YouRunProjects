618cb7689840c3cb88c06cb812c9fdc50822987c
donovanbrady <dduff484@gmail.com>
2018-07-23 16:08:26 -0400

    accomplished parsing the weight_percentages portion of the json string

--- weight_totals.cs
+++ weight_totals.cs
@@ -1,6 +1,8 @@
-using System;
+                                    using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,15 +32,16 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            //STEP 5                       
-                     
+            string jsonString = "{ \"weight_percentages\":[{\"water\":0.75},{\"hills\":0.5}],\"sections\":[{\"id\":\"1\",\"points\":[{\"latitude\":11.7575,\"longitude\":13.55567,\"weights\":{\"water\":1,\"hills\":2}},{\"latitude\":12.2343,\"longitude\":2.553535,\"weights\":{\"water\":3,\"hills\":8}}]},{\"id\":\"9\",\"points\":[{\"latitude\":44.7898,\"longitude\":90.5898,\"weights\":{\"water\":6,\"hills\":7}},{\"latitude\":60.2676,\"longitude\":32.553535,\"weights\":{\"water\":5,\"hills\":6}}]}]}";
+
+            parseJsonData(jsonString);
             //test values
             point point1 = new point(1, 2, 0);
             point point2 = new point(3, 3, 1);
             point point3 = new point(10, 10, 10);
             point point4 = new point(50, 0, 7);
             point point5 = new point(5, 11, 3);
-            
+
             List<point> points1 = new List<point>
             {
                 point1,
@@ -59,24 +62,24 @@ namespace HelloWorld
                 point4,
                 point5
             };
-                              
+
             //A list of lists that acts as a dynamic jagged array
             List<List<point>> sections = new List<List<point>>();
             sections.Add(points1);
             sections.Add(points2);
             sections.Add(points3);
             sections.Add(points4);
-                      
-            
+
+
             //a list of Hashtables that contains the results from getScannerResults
             List<Hashtable> section_results = new List<Hashtable>();
-        
-            
+
+
 
             //places the Scanner results from each pointer-array into section_results
             foreach (List<point> subList in sections)
             {
-                section_results.Add(getScannerResults(subList));                                
+                section_results.Add(getScannerResults(subList));
             }
 
             //A new hashtable with weight percentages for objects
@@ -89,7 +92,7 @@ namespace HelloWorld
             //a list containing the total values for each section when the weight percentages are factored in
             List<double> section_totals = new List<double>();
 
-            
+
             //runs a loop to add the totals from section_results into section_totals
             foreach (Hashtable hashtables in section_results)
             {
@@ -100,22 +103,24 @@ namespace HelloWorld
                     //makes the key in each DictionaryEntry a string and the Value a double
                     string key = (string)entry.Key;
                     double value_for_landmark = Convert.ToDouble(entry.Value);
-                                        
+
                     //adds the result of multiplying the weight percentages to the values for each landmark
-                     total += ((double)weight_percentages[key] * value_for_landmark);
+                    total += ((double)weight_percentages[key] * value_for_landmark);
 
                     //checks if the math adds up
                     //Console.WriteLine("This is the int value {0}", entry.Value);
-                   // Console.WriteLine("This is the weight_percentage {0}",(double)weight_percentages[key]);
-                   // Console.WriteLine("This is the value times the weight_percentage {0}", (double)weight_percentages[key] * value_for_landmark);
-                   //Console.WriteLine("{0}, {1}", entry.Key, entry.Value);
+                    // Console.WriteLine("This is the weight_percentage {0}",(double)weight_percentages[key]);
+                    // Console.WriteLine("This is the value times the weight_percentage {0}", (double)weight_percentages[key] * value_for_landmark);
+                    //Console.WriteLine("{0}, {1}", entry.Key, entry.Value);
                 }
                 section_totals.Add(total);
             }
             printHighestWeightedTotal(section_totals);
+
+            
             Console.ReadKey();
-                 }
-        
+        }
+
         //takes an array of pointers and stores the information in a hashtable
         //also adds the total values of all of the pointers
         static Hashtable getScannerResults(List<point> Points)
@@ -128,9 +133,9 @@ namespace HelloWorld
                 weight_water += p.water;
                 weight_hills += p.hills;
                 weight_sidewalk += p.sidewalk;
-            
+
             }
-            
+
             Hashtable pointWeights = new Hashtable();
             pointWeights["water"] = weight_water;
             pointWeights["hills"] = weight_hills;
@@ -172,7 +177,45 @@ namespace HelloWorld
             }
             Console.WriteLine("Section #{0} has the highest weighted total of {1}", highest_section, highest_weighted_total);
         }
-       
+
+        static void parseJsonData(string jsonString)
+        {   
+            ScannerData data;
+
+            JObject results = JObject.Parse(jsonString);
+
+            Dictionary<string, double> temp = new Dictionary<string, double>();
+            Dictionary<string, double> weight_percents = new Dictionary<string, double>();
+            
+            foreach (var element in results["weight_percentages"])
+            {
+                temp = JsonConvert.DeserializeObject<Dictionary<string, double>>(Convert.ToString(element));
+                
+                foreach (KeyValuePair<string, double> item in temp)
+                {
+                    weight_percents.Add(item.Key, item.Value);
+                }              
+                
+            }
+            data.weight_percentages = new Hashtable(weight_percents);
+
+           /* foreach (DictionaryEntry item in data.weight_percentages)
+            {
+                Console.WriteLine("Key {0}, Value {1}", item.Key, item.Value);
+            }
+            */
+                      
+             
+            //{"weight_percentages":[{"water":0.75},{"hills":0.5}],"sections"
+            //:[{"id":"1","points":[{"latitude":11.7575,"longitude":13.55567,
+            //"weights":{"water":1,"hills":2}},{"latitude":12.2343,"longitude":
+            // 2.553535,"weights":{"water":3,"hills":8}}]},
+            // {"id":"9","points":[{"latitude":44.7898,"longitude":90.5898,"weights":
+            // {"water":6,"hills":7}},{"latitude":60.2676,"longitude"
+            // :32.553535,"weights":{"water":5,"hills":6}}]}]}
+
+        }
+
     }
 
 }
