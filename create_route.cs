b37bf470de545bec53b5df060fee5c81993c23b2
donovanbrady <dduff484@gmail.com>
2018-08-08 13:43:59 -0400

    added the ability to identify if a point is within a section and then stores that point in that section

--- create_route.cs
+++ create_route.cs
@@ -31,31 +31,76 @@ namespace Data
         static void Main(string[] args)
         {
             List<Point> areaPoints = new List<Point>();
+            Point point1;
+            point1.longitude = 6;
+            point1.latitude = 1;
+            point1.weights = new Hashtable();
+            areaPoints.Add(point1);
             Point center;
-            center.longitude = 8;
-            center.latitude = 8;
+            center.longitude = 4;
+            center.latitude = 4;
             center.weights = new Hashtable();
-            divideSections(areaPoints, 6, 32, 16, 32, 16, center);
+            List<List<Point>> sections = new List<List<Point>>();
+            sections = divideSections(areaPoints, 4, 8, 0, 8, 0, center);
 
             Console.ReadKey();
 
         }
 
 
-        static void divideSections(List<Point> areaPoints, int numSections, double maxLat, double minLat, double maxLng,
+        static List<List<Point>> divideSections(List<Point> areaPoints, int numSections, double maxLat, double minLat, double maxLng,
                                          double minLng, Point center)
         {
+            int j = 1;
             int i = 1;
+            int z = 1;
             List<SectionSlice> sectionSlices = SlicetheSquarePie(areaPoints, numSections, maxLat, minLat, maxLng, minLng, center);
+
+            foreach (Point point in areaPoints)
+            {
+                Point currentPoint = point;
+
+                foreach (SectionSlice slice in sectionSlices)
+                {                    
+                    if (IsPointInBounds(currentPoint, slice) == true)
+                    {
+                        slice.memberPoints.Add(currentPoint);
+                        Console.WriteLine("Point {1} is within bounds of section {0}", i, z);
+                        break;
+                    }
+                    i++;
+                }
+
+                if (i > numSections)
+                {
+                    Console.WriteLine("point is not within selected area");
+                    areaPoints.Remove(currentPoint);
+                    break;
+
+                }
+                z++;
+            }
+
+            List<List<Point>> sections = new List<List<Point>>();
+
             foreach (SectionSlice slice in sectionSlices)
             {
-                Console.WriteLine("Section #{0}", i);
+                sections.Add(slice.memberPoints);
+            }
+
+            return sections;
+
+            /*foreach (SectionSlice slice in sectionSlices)
+            {
+                Console.WriteLine("Section #{0}", j);
                 foreach (Point point in slice.triangleBounds)
                 {
                     Console.WriteLine("longitude: {0}, latitude: {1}", point.longitude, point.latitude);
                 }
-                i++;
+                j++;
             }
+            */
+
 
         }
 
@@ -72,7 +117,7 @@ namespace Data
             Point point1;
             Point point2;
             SectionSlice slice;
-            double side_length = maxLat - minLat;                        
+            double side_length = maxLat - minLat;
             double perimeter = side_length * 4;
             double distance_between_points = perimeter / numSections;
             double current_distance = 0;
@@ -181,6 +226,72 @@ namespace Data
             return sectionSlices;
 
         }
+        static bool IsPointInBounds(Point currentpoint, SectionSlice bounds)
+        {
+            double y = currentpoint.latitude;
+            double x = currentpoint.longitude;
+            double[] latitudes = new double[3];
+            double[] longitudes = new double[3];
+            int i = 0;
+            foreach (Point edge in bounds.triangleBounds)
+            {
+                latitudes[i] = edge.latitude;
+                longitudes[i] = edge.longitude;
+                i++;
+            }
+            if (isInside(latitudes, longitudes, x, y) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /* A utility function to calculate area of triangle 
+   formed by (x1, y1) (x2, y2) and (x3, y3) */
+        static double area(double x1, double y1, double x2,
+                           double y2, double x3, double y3)
+        {
+            return Math.Abs((x1 * (y2 - y3) +
+                             x2 * (y3 - y1) +
+                             x3 * (y1 - y2)) / 2.0);
+        }
+
+        /* A function to check whether point P(x, y) lies
+        inside the triangle formed by A(x1, y1),
+        B(x2, y2) and C(x3, y3) */
+        static bool isInside(double[] latitude, double[] longitude, double x, double y)
+
+        {
+
+            double x1 = latitude[0];
+            double x2 = latitude[1];
+            double x3 = latitude[2];
+            double y1 = longitude[0];
+            double y2 = longitude[1];
+            double y3 = longitude[2];
+
+
+            /* Calculate area of triangle ABC */
+            double A = area(x1, y1, x2, y2, x3, y3);
+
+            /* Calculate area of triangle PBC */
+            double A1 = area(x, y, x2, y2, x3, y3);
+
+            /* Calculate area of triangle PAC */
+            double A2 = area(x1, y1, x, y, x3, y3);
+
+            /* Calculate area of triangle PAB */
+            double A3 = area(x1, y1, x2, y2, x, y);
+
+            /* Check if sum of A1, A2 and A3 is same as A */
+            return (A == A1 + A2 + A3);
+        }
+
+        /* Driver program to test above function */
+
+
 
     }
 }
