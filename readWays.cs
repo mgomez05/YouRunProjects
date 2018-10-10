11fbe0d7eeea073d97f9cfbacf781206cdb9d3d8
donovanbrady <dduff484@gmail.com>
2018-10-10 11:45:56 -0400

    Figured out how to read in IDs of ways and IDs of their nd children nodes

--- readWays.cs
+++ readWays.cs
@@ -1,15 +1,35 @@
-      Hashtable points;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+
+namespace YouRunShared.iOS
+
+{
+    public class WayReader
+    {
+        public static void Main(string[] args)
+        {
+            readWays();
+        }
+
+        public static void readWays()
+        {
+            //Hashtable points;
 
             // Create new Xml Document
             System.Xml.XmlDocument xDoc = new System.Xml.XmlDocument();
 
-            //xDoc.Load("/Users/shehryarmalik/Documents/youruntesting/MyFirstProject/YouRunShared/iOS/test2.xml");
-
             // Load the file as an xml document
-            xDoc.Load(_path + "test2.xml");
+            xDoc.Load("C:/Users/dduff/Scanner/OSMFileWithWays.xml");
 
             // Get elements with tag name "node" and put them in nodeTree
-            System.Xml.XmlNodeList nodeTree = xDoc.GetElementsByTagName("node");
+            System.Xml.XmlNodeList nodeTree = xDoc.GetElementsByTagName("way");
 
             // Check the method loaded at least 1 node
             if (nodeTree.Count == 0)
@@ -17,10 +37,31 @@
                 Debug.WriteLine("node Tree is NULL");
             }
 
-            // Check if data was read in correctly
-            WriteNodesAndTagsToFile(nodeTree);
+            // for each way
+            //  -read in important attributes (it's ID)
+            //  -read in each child node reference & their attributes
+            /// -READ IN TAGS AND KEY VALUE PAIRS
+            //  -post it to database
 
-            // Convert the tree to a hashtable
-            points = AddTreeToHashtable(nodeTree);
+            foreach (XmlNode node in nodeTree)
+            {
+                // Select children of "node" that have the tag "nd"
+                XmlNodeList childNodes = node.SelectNodes(".//nd");
 
-            return points;
+                String name = node.Attributes["id"].Value;
+
+                Debug.WriteLine("Here are " + name + "'s children");
+
+                foreach (XmlNode childNode in childNodes)
+                {
+                    Debug.WriteLine("child node " + childNode.Attributes["ref"].Value);
+                }
+
+                
+
+                Debug.WriteLine("");
+            }
+        }
+    }
+
+}
